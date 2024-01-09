b568077a46f0f81d023ca20b2fcd100237d56f43
Anna Zadiraka <annazadiraka@gmail.com>
2024-01-09 18:25:23 +0300

    add all section

--- js/main.js
+++ js/main.js
@@ -6,3 +6,20 @@ $burgerBtn.addEventListener('click', () => {
     $burgerBtn.classList.toggle('open');
     $menu.classList.toggle('open');
 })
+
+// Swiper
+const swiper = new Swiper('.project__swiper', {   
+   
+    direction: 'horizontal',
+    loop: true,
+    slidesPerView: 1,  
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+  });
+
+// Галерея секции Интерьер
+Fancybox.bind('[data-fancybox="gallery"]', {
+    
+});
